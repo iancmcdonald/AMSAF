c56595d5275f5a4c805c82fcf39b2ba8c5e62b1c
ian <ian.c.mcdonald5@gmail.com>
2017-08-04 11:43:50 -0700

    updated amsaf_functional

--- amsaf/amsaf_functional.py
+++ amsaf/amsaf_functional.py
@@ -48,26 +48,28 @@ def subtraction_evaluator(ground_truth, seg):
 
 
 def generate_parameter_map():
-    elastix_params = {}  # parameters go here
-    param_grid = ParameterGrid(elastix_params)
+    # elastix_params = {}  # parameters go here
+    # param_grid = ParameterGrid(elastix_params)
+    param_grid = [sitk.GetDefaultParameterMap('translation')]
 
     for param_map in param_grid:
         yield param_map
 
 
-def segment_image(ref_image_crop_list, ref_seg_crop_list, ref_image_ground_truth_crop, ref_seg_ground_truth_crop,
-                  target_image_crop_list, target_image_ground_truth_crop, target_seg_ground_truth_crop,
-                  parameter_priors):
+def optimize_parameter_map(ref_image_ground_truth_crop, ref_seg_ground_truth_crop, target_image_ground_truth_crop,
+                           target_seg_ground_truth_crop, parameter_priors):
     def get_seg_score_and_transform_parameter_map(parameter_map):
         transform_parameter_map = get_transform_parameter_map(target_image_ground_truth_crop,
                                                               ref_image_ground_truth_crop, parameter_map)
 
         result_seg = apply_transformation(ref_seg_ground_truth_crop, transform_parameter_map)
+
         seg_score = subtraction_evaluator(target_seg_ground_truth_crop, result_seg)
 
         return seg_score, transform_parameter_map
 
     # TODO(Ian): Replace imap with numap or Multiprocess imap
-
     best_parameter_map = max(imap(get_seg_score_and_transform_parameter_map, generate_parameter_map()),
                              key=lambda pair: pair[0])
+
+    print(best_parameter_map)
