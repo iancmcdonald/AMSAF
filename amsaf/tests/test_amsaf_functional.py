e27c18b8821ad9dc625a6535a09a711ccb6a2926
Daniel <ho@berkeley.edu>
2017-08-08 11:22:26 -0700

    merged test file

--- amsaf/tests/test_amsaf_functional.py
+++ amsaf/tests/test_amsaf_functional.py
@@ -25,30 +25,6 @@ test_img1 = sitk.ReadImage("/home/daniel/mridata/UnitTest/combined_seg.nii")
 
 test_img2 = sitk.ReadImage("/home/daniel/mridata/UnitTest/transformation_result.nii")
 class TestAmsafFunctional(unittest.TestCase):
-<<<<<<< HEAD
-    # def test_get_transform_parameter_map(self):
-    #     parameter_map = sitk.GetDefaultParameterMap('rigid')
-    #     parameter_map['AutomaticTransformInitialization'] = ['true']
-    #     get_transform_parameter_map(sub3_forearm_img_cropped, PQ_forearm_img_cropped, parameter_map)
-    #
-    #     self.assertEqual(True, False)
-
-    def test_subtraction_evaluator(self):
-        # Note: Dimensions are 294 x 871 x 305
-
-        # Current problem: It looks like the double values are maxing out (1.7e+308), and Dice will become inf
-        autoseg = test_img2
-        groundtruth = sitk.Image(294, 871, 305, 3)
-        groundtruth.SetOrigin(autoseg.GetOrigin())
-        groundtruth.SetSpacing(autoseg.GetSpacing())
-        groundtruth.SetDirection(autoseg.GetDirection())
-        expected = subtraction_evaluator(groundtruth, autoseg)
-        self.assertEqual(expected, 78102570)
-
-    # def test_identity(self):
-    #     identity = test_img1 - test_img2
-    #     self.assertEqual(identity, test_img1)
-=======
     def test_get_transform_parameter_map(self):
         parameter_map = sitk.GetDefaultParameterMap('rigid')
         parameter_map['AutomaticTransformInitialization'] = ['true']
@@ -70,7 +46,6 @@ class TestAmsafFunctional(unittest.TestCase):
         seg_result = setup()[1]
         sub_score = subtraction_evaluator(sub3_forearm_muscles_ground_truth, seg_result)
         self.assertTrue(sub_score)
->>>>>>> 2571f83bb9bc681a6e1defa06b37e303c62cbbd5
 
 
 if __name__ == '__main__':
