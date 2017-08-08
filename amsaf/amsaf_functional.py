6087bcc07cf08d9769970661ac023fd4fee2105b
ian <ian.c.mcdonald5@gmail.com>
2017-08-08 13:14:57 -0700

    debugging parameter map generator

--- amsaf/amsaf_functional.py
+++ amsaf/amsaf_functional.py
@@ -67,34 +67,62 @@ def dice_evaluator(ground_truth, seg):
     return overlapFilter.GetDiceCoefficient()
 
 
-def generate_parameter_maps(priors):
+def generate_parameter_maps(priors=None):
+    # priors is a list of tuples [()]
     def get_parameter_options_dict(transform):
         if transform == 'rigid':
             param_grid = {
                 'AutomaticParameterEstimation': ['true'],
                 'AutomaticTransformInitialization': ['true'],
+                'AutomaticTransformInitializationMethod': ['CenterOfGravity', 'Origins', 'GeometricalCenter',
+                                                           'GeometryTop'],
+                'BSplineInterpolationOrder': ['1', '3'],
                 'CheckNumberOfSamples': ['true'],
                 'DefaultPixelValue': ['0'],
                 'FinalBSplineInterpolationOrder': ['3'],
                 'FixedImagePyramid': ['FixedSmoothingImagePyramid', 'FixedRecursiveImagePyramid'],
-
+                'ImagePyramidSchedule': ['2 2 1', '4 4 1  2 2 1  1 1 1', '32 32 2  16 16 2',
+                                         '16 16 2  8 8 1  4 4 1  2 2 1  1 1 1'],
+                'Interpolator': ['BSplineInterpolator'],
+                'MaximumNumberOfIterations': ['512', '1024', '2048'],
+                'Metric': ['AdvancedMattesMutualInformation', 'AdvancedNormalizedCorrelation',
+                           'NormalizedMutualInformation'],
+                'NumberOfHistogramBins': ['32', '64', '128', '16 32 64', '64 32 16'],
+                'NumberOfResolutions': ['1', '2', '3', '6'],
+                'NumberOfSpatialSamples': ['2000', '4000', '8000'],
+                'Registration': ['MultiResolutionRegistration'],
+                'Resampler': ['DefaultResampler'],
+                'ResampleInterpolator': ['FinalBSplineInterpolator']
             }
 
-    def convert_to_elastix(params):
-        pass
+            if priors:
+                for key, val in priors:
+                    param_grid[key] = [val]
 
-    # elastix_params = {}  # parameters go here
-    # param_grid = ParameterGrid(elastix_params)
-    translation_map = sitk.GetDefaultParameterMap('rigid')
-    translation_map['AutomaticTransformInitialization'] = ['true']
-    param_grid = [translation_map]
+            return param_grid
+
+    def convert_to_elastix(param_dict):
+        # type: (dict) -> sitk.ParameterMap
+        elastix_param_map = sitk.ParameterMap()
+        for param, val in param_dict.iteritems():
+            elastix_param_map[param] = [val]
+
+        return elastix_param_map
+
+    param_grid = ParameterGrid(get_parameter_options_dict('rigid'))
+
+    # for testing
+    # default_map = sitk.GetDefaultParameterMap('rigid')
+    # default_map['AutomaticTransformInitialization'] = ['true']
+    # param_grid = [default_map]
 
     for param_map in param_grid:
-        yield param_map
+        yield convert_to_elastix(param_map)
+        # yield param_map
 
 
 def optimize_parameter_map(ref_image_ground_truth_crop, ref_seg_ground_truth_crop, target_image_ground_truth_crop,
-                           target_seg_ground_truth_crop, parameter_priors):
+                           target_seg_ground_truth_crop, parameter_priors=None):
     def process_seg_result(ground_truth, seg):
         processed_seg = sitk.Cast(seg, ground_truth.GetPixelID())
         ground_truth.CopyInformation(processed_seg)
@@ -112,7 +140,7 @@ def optimize_parameter_map(ref_image_ground_truth_crop, ref_seg_ground_truth_cro
         return seg_score, transform_parameter_map
 
     # TODO(Ian): Replace imap with numap or Multiprocess imap
-    best_parameter_map = max(imap(get_seg_score_and_transform_parameter_map, generate_parameter_maps(parameter_priors)),
+    best_parameter_map = max(imap(get_seg_score_and_transform_parameter_map, generate_parameter_maps()),
                              key=lambda pair: pair[0])
 
     return best_parameter_map
